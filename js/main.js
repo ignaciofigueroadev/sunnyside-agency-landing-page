e7868da6ff252766c4250bace327d97cc93e54d6
Figueroa <figueroaignaciodev@gmail.com>
2023-03-14 13:25:18 -0300

    Adding the features section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 const hamburguerButton = document.querySelector(".header__toggle");
 const navBar = document.querySelector(".header__nav");
+const navLink = document.querySelectorAll(".header__li");
 
 hamburguerButton.addEventListener("click", () => {
   navBar.classList.toggle("header__nav--active");
 });
+
+navLink.forEach((n) =>
+  n.addEventListener("click", () => {
+    navBar.classList.remove("header__nav--active");
+  })
+);
